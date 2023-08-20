4ef521c4f39c682d0adf97377017feaa080fb8ff
Nawadit Sharma <93001090+prozacnzoloft@users.noreply.github.com>
2023-08-20 23:23:54 +0545

    Delete Journal/Day 8 directory

--- Journal/Day 8/npm/installingAndUsingPackages/index.js
+++
@@ -1,9 +0,0 @@
-// const { upperCase } = require("upper-case")
-const upperCase = require('nawadit-uppercase')
-
-const toUpperCase = (string)=>{
-    console.log(upperCase(string))
-    return upperCase(string)
-}
-// toUpperCase('this string  is in uppercase')
-module.exports = toUpperCase
